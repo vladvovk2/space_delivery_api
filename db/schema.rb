ffc8435f435812e4fb6d6e858d2c2c131a923467
vladvovk2 <vlad.vovk.844@gmail.com>
2019-02-06 16:53:23 +0200

    Addet to Gemfile rubocop-rails_config gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,96 +11,95 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_02_04_233801) do
-
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "carts", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'carts', force: :cascade do |t|
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string "title"
-    t.string "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'categories', force: :cascade do |t|
+    t.string 'title'
+    t.string 'image'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "line_items", force: :cascade do |t|
-    t.integer "quantity", default: 1
-    t.bigint "product_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "cart_id"
-    t.bigint "order_id"
-    t.index ["cart_id"], name: "index_line_items_on_cart_id"
-    t.index ["order_id"], name: "index_line_items_on_order_id"
-    t.index ["product_id"], name: "index_line_items_on_product_id"
+  create_table 'line_items', force: :cascade do |t|
+    t.integer 'quantity', default: 1
+    t.bigint 'product_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.bigint 'cart_id'
+    t.bigint 'order_id'
+    t.index ['cart_id'], name: 'index_line_items_on_cart_id'
+    t.index ['order_id'], name: 'index_line_items_on_order_id'
+    t.index ['product_id'], name: 'index_line_items_on_product_id'
   end
 
-  create_table "orders", force: :cascade do |t|
-    t.string "first_name"
-    t.string "second_name"
-    t.string "adress"
-    t.string "user_number"
-    t.string "total_price"
-    t.string "delivery_type"
-    t.string "pay_type"
-    t.text "description"
-    t.boolean "status", default: false
-    t.string "promo_code"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_orders_on_user_id"
+  create_table 'orders', force: :cascade do |t|
+    t.string 'first_name'
+    t.string 'second_name'
+    t.string 'adress'
+    t.string 'user_number'
+    t.string 'total_price'
+    t.string 'delivery_type'
+    t.string 'pay_type'
+    t.text 'description'
+    t.boolean 'status', default: false
+    t.string 'promo_code'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.bigint 'user_id'
+    t.index ['user_id'], name: 'index_orders_on_user_id'
   end
 
-  create_table "products", force: :cascade do |t|
-    t.string "title"
-    t.string "image"
-    t.string "description"
-    t.string "weight"
-    t.integer "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "category_id"
-    t.index ["category_id"], name: "index_products_on_category_id"
+  create_table 'products', force: :cascade do |t|
+    t.string 'title'
+    t.string 'image'
+    t.string 'description'
+    t.string 'weight'
+    t.integer 'price'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.bigint 'category_id'
+    t.index ['category_id'], name: 'index_products_on_category_id'
   end
 
-  create_table "promo_codes", force: :cascade do |t|
-    t.integer "amount", default: 40
-    t.string "code"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["code"], name: "index_promo_codes_on_code", unique: true
-    t.index ["user_id"], name: "index_promo_codes_on_user_id"
+  create_table 'promo_codes', force: :cascade do |t|
+    t.integer 'amount', default: 40
+    t.string 'code'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.bigint 'user_id'
+    t.index ['code'], name: 'index_promo_codes_on_code', unique: true
+    t.index ['user_id'], name: 'index_promo_codes_on_user_id'
   end
 
-  create_table "user_balances", force: :cascade do |t|
-    t.integer "balance", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_user_balances_on_user_id"
+  create_table 'user_balances', force: :cascade do |t|
+    t.integer 'balance', default: 0
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.bigint 'user_id'
+    t.index ['user_id'], name: 'index_user_balances_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "number", null: false
-    t.string "user_name", null: false
-    t.string "image"
-    t.string "password_digest", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', null: false
+    t.string 'number', null: false
+    t.string 'user_name', null: false
+    t.string 'image'
+    t.string 'password_digest', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  add_foreign_key "line_items", "carts"
-  add_foreign_key "line_items", "orders"
-  add_foreign_key "line_items", "products"
-  add_foreign_key "orders", "users"
-  add_foreign_key "products", "categories"
-  add_foreign_key "promo_codes", "users"
-  add_foreign_key "user_balances", "users"
+  add_foreign_key 'line_items', 'carts'
+  add_foreign_key 'line_items', 'orders'
+  add_foreign_key 'line_items', 'products'
+  add_foreign_key 'orders', 'users'
+  add_foreign_key 'products', 'categories'
+  add_foreign_key 'promo_codes', 'users'
+  add_foreign_key 'user_balances', 'users'
 end
