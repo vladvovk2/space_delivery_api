4a8518e8acffcdedeb9044aec87a226ea4cf4ef2
vladvovk2 <vlad.vovk.844@gmail.com>
2019-03-27 15:01:49 +0200

    Added test for POST /api/users endpoint

--- app/api/delivery_api/entities/user_responce.rb
+++ app/api/delivery_api/entities/user_responce.rb
@@ -7,16 +7,16 @@ module DeliveryApi
       expose :email,           documentation: { type: 'String',   values: ['vlad.vovk.844@gmail.com'] }
       expose :email_confirm,   documentation: { type: 'Boolean',  values: ['true'] }
       expose :phone_number,    documentation: { type: 'String',   values: ['+380687285102'] }
-      expose :user_balance,    documentation: { type: 'Integer',  values: ['121'] }
-      expose :user_promo_code, documentation: { type: 'String',   values: ['3F1ds3k1jc'] }
+      expose :balance,    documentation: { type: 'Integer',  values: ['121'] }
+      expose :promo_code, documentation: { type: 'String',   values: ['3F1ds3k1jc'] }
 
       private
 
-      def user_balance
+      def balance
         object.user_balance.balance
       end
 
-      def user_promo_code
+      def promo_code
         object.promo_code.code
       end
     end
