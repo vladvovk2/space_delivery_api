47b3e817365cbe68e09740d1860d71bad323c3e8
vladvovk2 <vlad.vovk.844@gmail.com>
2019-03-31 16:14:00 +0300

    Added total_price method yo line_item model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,6 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
+  belongs_to :user, optional: true
 
   def add_product(product)
     item = line_items.find_by(product_type: product)
