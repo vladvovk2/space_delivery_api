e2bc368a365e3268b8e7b6e02fc0f715dc9de267
vladvovk2 <vlad.vovk.844@gmail.com>
2019-04-04 23:08:56 +0300

    Added split_address method

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,4 +3,10 @@ class Place < ApplicationRecord
 
   reverse_geocoded_by :latitude, :longitude
   after_validation :reverse_geocode
+
+  before_save :split_address
+
+  def split_address
+    self.address = address.split(', Черкассы').first
+  end
 end
