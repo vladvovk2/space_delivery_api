c34c1eefb41f2bd4c4631bb7afc0f47dc977ba0e
vladvovk2 <vlad.vovk.844@gmail.com>
2019-04-06 17:51:54 +0300

    Added user_registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,25 @@ class UsersController < ApplicationController
       redirect_to root_url
     end
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = 'Created.'
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :phone_number, :password, :password_confirmation)
+  end
 end
