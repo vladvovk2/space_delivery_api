98350a785a4cd5ac3b1c0b6e7fbe1dcf4d51b794
FalseNumber <sasha.sanya.aleksandr@gmail.com>
2019-04-07 17:22:13 +0300

    fixed bug that only one tab can be opened

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,20 +1,21 @@
-function openDetails(evt, tabNum) {
+function openDetails(evt, tabContentNum) {
   // Declare all variables
   var i, tabcontent, tablinks;
+  var product = document.getElementById(tabContentNum).parentElement;
 
   // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
+  tabcontent = product.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
   // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
+  tablinks = product.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(tabNum).style.display = "flex";
+  document.getElementById(tabContentNum).style.display = "flex";
   evt.currentTarget.className += " active";
 }
