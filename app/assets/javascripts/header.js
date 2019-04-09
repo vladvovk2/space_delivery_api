38a1cd31fd9e6484b0721a0e9734809ea3ea4194
FalseNumber <sasha.sanya.aleksandr@gmail.com>
2019-04-09 23:51:32 +0300

    made navigation icons sticky. edited menu items of Profile

---
+++ app/assets/javascripts/header.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+ // alert($(".sub-menu").height());
+  if ($(".sub-menu").css("height") === "0") {
+    $(".profile").css("background-color", "red");
+  }
+  //$("div").hide();
+  // if ($(".sub-menu").attr("height") === 0) {
+  //   $(".profile").attr("background-color", "red");
+  // }
+});
