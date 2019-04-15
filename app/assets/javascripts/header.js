be3d3675397a44aee13c3c8242a3eb567078ccd4
Vladislav <vlad.vovk.844@gmail.com>
2019-04-15 19:55:25 +0300

    Fixed bug with jumping icons on hovering

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -4,7 +4,7 @@ $(document).on('turbolinks:load', function(){
   $(".profile").mouseleave(function() {
     timeOut = setTimeout(function() {
      $(".profile").css("background-color", "transparent");
-     $(".profile").css("border", "none");
+     $(".profile").css("border-color", "transparent");
      $(".profile .content").css("visibility", "visible");
     }, 305);
   });
@@ -12,7 +12,7 @@ $(document).on('turbolinks:load', function(){
     clearTimeout(timeOut);
     $(".profile").css("background-color", "rgba(77, 77, 77, 0.705)");
     $(".profile").css("border", "1px solid #ffffff");
-    $(".profile").css("border-bottom", "none");
+    $(".profile").css("border-bottom-color", "transparent");
     $(".profile a .content").css("visibility", "hidden");
   });
 });
