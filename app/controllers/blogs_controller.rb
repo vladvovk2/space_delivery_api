c5b5e2ff3b2f55af6205aaeaa9dedc07ef91e9a4
vladvovk2 <vlad.vovk.844@gmail.com>
2019-04-16 03:25:03 +0300

    Created worker

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,6 +2,24 @@ class BlogsController < ApplicationController
   def index; end
 
   def download_menu
-    GenerateMenuWorker.perform_async("current_user")
+    GenerateMenuWorker.perform_async
+  end
+
+  private
+
+  def generate_pdf
+    Prawn::Document.generate('public/pdfs/menu_4.pdf') do |pdf|
+      Category.includes(products: :product_types).all.each do |category|
+        pdf.text "Category: #{category.title}", align: :center, size: 20
+        category.products.each do |product|
+          pdf.text "Product: #{product.title}", size: 15
+          product.product_types.each do |pt|
+            pdf.text "proportion: #{pt.proportion}, price: #{pt.price}, weight: #{pt.weight}", size: 10
+          end
+          pdf.text '-' * 50
+        end
+      end
+      pdf.render
+    end
   end
 end
--- app/workers/generate_menu_worker.rb
+++ app/workers/generate_menu_worker.rb
@@ -3,12 +3,8 @@ require 'prawn'
 class GenerateMenuWorker
   include Sidekiq::Worker
 
-  def perform(user)
-    generate_pdf(user)
-  end
-
-  def generate_pdf(string)
-    Prawn::Document.generate("/public/menu_#{string}.pdf") do |pdf|
+  def perform
+    Prawn::Document.generate('public/pdfs/menu_5.pdf') do |pdf|
       Category.includes(products: :product_types).all.each do |category|
         pdf.text "Category: #{category.title}", align: :center, size: 20
         category.products.each do |product|
