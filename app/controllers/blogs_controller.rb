05c42ba73ddd935fb347a67c253f07e36074be5c
vladvovk2 <vlad.vovk.844@gmail.com>
2019-04-16 18:53:07 +0300

    Added path_indentifier

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,13 +1,22 @@
 class BlogsController < ApplicationController
+  before_action :path_identifier, only: %i[generate_menu download_menu]
+
   def index; end
 
   def generate_menu
-    MenuJob.new.perform
+    path = "#{Rails.root}/public/pdfs/menu_#{@path_identifier}.pdf"
+    MenuJob.new.perform(@path_identifier) unless File.exist?(path)
   end
 
   def download_menu
-    send_file "#{Rails.root}/uploads/pdfs/menu_#{}.pdf",
-              type: 'application/pdf',
+    send_file "#{Rails.root}/public/pdfs/menu_#{@path_identifier}.pdf",
+              type: 'pdf',
               x_sendfile: true
+  end
+
+  private
+
+  def path_identifier
+    @path_identifier ||= current_user.phone_number.last(7)
   end
 end
--- app/jobs/menu_job.rb
+++ app/jobs/menu_job.rb
@@ -1,18 +1,18 @@
 class MenuJob
-  def perform
-    @time = Time.now.strftime('%F-%R')
+  def perform(identifier)
+    @identifier = identifier
 
     batch = Sidekiq::Batch.new
     batch.callback_queue = 'messages'
-    batch.on(:success, MenuJob::Create, @time)
+    batch.on(:success, MenuJob::Create, @identifier)
     batch.jobs do
-      GenerateMenuWorker.perform_async(@time)
+      GenerateMenuWorker.perform_async(@identifier)
     end
   end
 
   class Create
-    def on_success(_status, time)
-      DeleteMenuWorker.perform_in(10.minutes, time)
+    def on_success(_status, identifier)
+      DeleteMenuWorker.perform_in(10.minutes, identifier)
     end
   end
 end
