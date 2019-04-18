973752624ad8dcb5558ef82113ae503aa591b725
vladvovk2 <vlad.vovk.844@gmail.com>
2019-04-18 22:02:51 +0300

    Added buy_together endpoint for cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,8 @@
 class CartsController < ApplicationController
   def show
     @cart = Cart.includes(line_items: [product_type: [product: :picture]]).find(session[:cart_id])
+
+    buy_together
   end
 
   def add_product
@@ -10,5 +12,16 @@ class CartsController < ApplicationController
     respond_to do |format|
       format.js
     end
+  end
+
+  def buy_together
+    ids = []
+
+    current_cart.line_items.map do |ln|
+      ids << ln.product_type.product.id
+    end
+
+    product_ids = ProductSale.where(active_id: ids.uniq).order(sales_count: :desc).limit(3).pluck(:passive_id)
+    @products = Product.find(product_ids)
   end
 end
