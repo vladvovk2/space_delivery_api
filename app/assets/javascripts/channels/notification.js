150068f82b48db76e0ebccfbfe4686f7cf5e73b7
Vladislav <vlad.vovk.844@gmail.com>
2019-04-23 21:41:02 +0300

    Added smooth animation for notifications

--- app/assets/javascripts/channels/notification.js
+++ app/assets/javascripts/channels/notification.js
@@ -2,7 +2,15 @@ App.notifications = App.cable.subscriptions.create('NotificationsChannel', {
   received: function(data) {
     $("#notification-messages").removeClass('hidden')
     var notificationContent = $('#notification-messages').append(this.renderNotification(data));
-    $('.notification-wrapper').fadeOut(2500);
+    var notificationWrapper = $('.notification-wrapper:last-child');
+    
+    setTimeout(function() {
+      notificationWrapper.animate({opacity: 0.1}, 2250, function() {
+          notificationWrapper.css('opacity', '0');
+          notificationWrapper.hide(500);
+      });
+    }, 1000);  
+
     return notificationContent;
   },
 
