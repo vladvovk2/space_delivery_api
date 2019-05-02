cee8130b63c39b1816cc0e41358f93721758c4d2
Vladislav <vlad.vovk.844@gmail.com>
2019-05-02 23:19:24 +0300

    Aligned elements inside of suggested-tabcontent. Made text white

---
+++ app/assets/javascripts/carts.js
@@ -0,0 +1,21 @@
+function openSuggDetails(evt, tabContentNum) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+  var product = document.getElementById(tabContentNum).parentElement;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = product.getElementsByClassName("suggested-tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = product.getElementsByClassName("suggested-tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabContentNum).style.display = "flex";
+  evt.currentTarget.className += " active";
+}
