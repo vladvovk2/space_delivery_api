94439b247063b0b3b1e3cb112991f511b054daad
vladvovk2 <vlad.vovk.844@gmail.com>
2019-05-06 19:25:22 +0300

    fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,5 @@ if Rails.env.development?
   AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 end
 
-AdminUser.create!(email: 'vlad.vovk.884@gmal.com', password: 'vladvovk2', password_confirmation: 'vladvovk2')
+AdminUser.create!(email: 'vlad.vovk.844@gmail.com', password: 'vladvovk2', password_confirmation: 'vladvovk2')
 Category.create(title: 'Burgers')
