4df4ddd7c013ce1af6fbc7fd53cb3bdd767749e5
vladvovk2 <vlad.vovk.844@gmail.com>
2019-05-06 17:14:56 +0300

    fixes

--- ansible/up_project.sh
+++ ansible/up_project.sh
@@ -1,10 +1,9 @@
 cd /home/app/ma/current
 
-docker-compose -f docker-compose.production.yml stop
-docker-compose -f docker-compose.production.yml rm -f -v
+docker-compose -f docker-compose.production.yml down
 docker-compose -f docker-compose.production.yml build
 docker-compose -f docker-compose.production.yml run --rm rails rake assets:precompile && rake db:migrate
-docker-compose -f docker-compose.production.yml stop
+docker-compose -f docker-compose.production.yml down
 docker-compose -f docker-compose.production.yml up -d
 sleep 5
 docker ps
